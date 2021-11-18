0b4e8d7fbc09d385cc3cf7046e1a1d5ea1204723
Andrey <69643088+AndySLO@users.noreply.github.com>
2021-11-18 14:51:49 +0100

    Initial Commit

---
+++ MathGame App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathGame App
+//
+//  Created by AndreyP on 18/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MathGame App/MathGame_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGame_AppApp.swift
+//  MathGame App
+//
+//  Created by AndreyP on 18/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MathGame_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
